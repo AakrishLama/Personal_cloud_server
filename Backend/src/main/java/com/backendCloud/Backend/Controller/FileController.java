fc1ec85b30cbfcd68c896f1002f7287409286f6b
Aakrish Lama <al226na@student.lnu.se>
2025-09-06 21:00:35 +0200

    rebase and cors in file

--- Backend/src/main/java/com/backendCloud/Backend/Controller/FileController.java
+++ Backend/src/main/java/com/backendCloud/Backend/Controller/FileController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import com.backendCloud.Backend.Model.FileDocument;
 import com.backendCloud.Backend.Service.FileService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/files")
 public class FileController {
 
